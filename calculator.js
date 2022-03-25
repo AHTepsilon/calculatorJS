d0c67edd934cd5d9a1dcf909f0fe24a66093e2b9
Alejandro Hernandez Toro <1006009372@u.icesi.edu.co>
2022-03-25 08:33:16 -0500

    managed to complete each operation

--- calculator.js
+++ calculator.js
@@ -27,9 +27,45 @@ function divide(num1, num2){
 
 }
 
-total1 = sum(arr[0], arr[1]);
-total2 = sum(arr[2], arr[3]);
+totalFinal = arr[0];
+resultFinal = arr[0];
+productFinal = arr[0];
+quotientFinal = arr[0];
 
-totalFinal = sum(total1, total2);
+arr.forEach((number, index) =>{
+    
+    if(index > 0){
 
-console.log(totalFinal);
+        totalFinal = sum(totalFinal, number);
+
+        if(totalFinal == 70){
+            console.log(totalFinal);
+        }
+
+    }
+
+    if(index > 0){
+        resultFinal = substract(resultFinal, number);
+
+        if(resultFinal == -50){
+            console.log(resultFinal);
+        }
+    }
+
+    if(index > 0){
+        productFinal = multiply(productFinal, number);
+
+        if(productFinal == 37500){
+            console.log(productFinal);
+        }
+    }
+
+    if(index > 0){
+        quotientFinal = divide(quotientFinal, number);
+
+        if(quotientFinal == 0.0026666666666666666){
+            console.log(quotientFinal);
+        }
+        
+    }
+    });
